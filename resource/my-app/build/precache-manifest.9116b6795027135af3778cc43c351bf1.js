b3593c4386185097990f8c472c86fde936d51dc7
lalona <sebastianxiro@gmail.com>
2021-08-06 16:05:18 -0600

    Upload de build.

---
+++ resource/my-app/build/precache-manifest.9116b6795027135af3778cc43c351bf1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9266042a6210b585a6ba94552729d136",
+    "url": "../../resource/my-app/build/index.html"
+  },
+  {
+    "revision": "ae987e6522f6ad46c9f3",
+    "url": "../../resource/my-app/build/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "ad075e68631f83e25fbf",
+    "url": "../../resource/my-app/build/static/js/2.2f5ef2c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "../../resource/my-app/build/static/js/2.2f5ef2c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae987e6522f6ad46c9f3",
+    "url": "../../resource/my-app/build/static/js/main.0d454db1.chunk.js"
+  },
+  {
+    "revision": "143654bf4924e1b439bb",
+    "url": "../../resource/my-app/build/static/js/runtime-main.136f4e2d.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "../../resource/my-app/build/static/media/logo.ee7cd8ed.svg"
+  }
+]);
